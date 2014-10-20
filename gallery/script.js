f45cd3192c6a56f7a37a06bb64994ae0e729773a
Andy <rosario.andrew@gmail.com>
2014-10-20 18:41:06 -0500

    Animal stuff

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,20 @@
-// Add your code here
+
+
+$(".button").on("click", function() {
+	var animal = ($(event.target).attr("id"));
+	$("." + animal).slideToggle();
+	var toCap = function(word) {
+		return word.charAt(0).toUpperCase() + word.slice(1);
+	};
+	var capitalized = toCap(animal);
+	alertify.log("Toggling " + capitalized);
+});
+
+
+$(".button").on('click', function() {
+	$(this).toggleClass("active");
+});
+
+// find out what was clicked
+// change the id to a class
+// toggle the class
